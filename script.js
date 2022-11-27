88de14777cfbf0f601f689054835ce586e5a9026
malik <boughris@outlook.fr>
2022-11-27 01:29:14 +0000

    implemented the basic functionalities

--- script.js
+++ script.js
@@ -1,8 +1,60 @@
-const keyNumber = document.querySelectorAll('.number');
-const output = document.getElementById('output');
+const keyNumber = document.querySelectorAll(".number");
+const output = document.getElementById("output");
+const reset = document.getElementById("reset");
+const del = document.getElementById("delete");
+const operation = document.querySelectorAll(".operation");
+const equal = document.getElementById("equal");
 
-keyNumber.forEach(e => {
-    e.addEventListener('click', num => {
-        output.value = num.target.value
-    })
-})
+const calculator = () => {
+  const calculations = {
+    previous: 0,
+    current: 0,
+    operand: "",
+  };
+
+  keyNumber.forEach((num) => {
+    num.addEventListener("click", (e) => {
+      output.value += e.target.value;
+    });
+  });
+
+  del.addEventListener("click", () => {
+    output.value = null;
+  });
+
+  reset.addEventListener("click", () => {
+    calculations.previous = 0;
+    calculations.current = 0;
+    calculations.operand = "";
+    output.value = null;
+  });
+
+  operation.forEach((ope) => {
+    ope.addEventListener("click", (e) => {
+      calculations.operand = e.target.value;
+      calculations.previous = output.value;
+      output.value = null;
+    });
+  });
+
+  equal.addEventListener("click", () => {
+    calculations.current = output.value;
+    switch (calculations.operand) {
+      case "+":
+        output.value =
+          parseInt(calculations.previous) + parseInt(calculations.current);
+        break;
+      case "-":
+        output.value = parseInt(calculations.previous) - parseInt(calculations.current);
+        break;
+      case "x":
+        output.value = parseInt(calculations.previous) * parseInt(calculations.current);
+        break;
+      case "/":
+        output.value = parseInt(calculations.previous) / parseInt(calculations.current);
+        break;
+    }
+  });
+};
+
+calculator();
